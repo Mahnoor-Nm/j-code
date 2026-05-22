7e2252321362e5243c688ea5be9cd8154f7fbe10
Mahnoor nadeem <mano.nadeem55@gmail.com>
2026-05-22 10:03:22 +0500

    Add files via upload

---
+++ s.java
@@ -0,0 +1,12 @@
+package src;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Your Jenkins CI/CD pipeline is working perfectly.");
+    }
+
+    // A simple method we can test
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
